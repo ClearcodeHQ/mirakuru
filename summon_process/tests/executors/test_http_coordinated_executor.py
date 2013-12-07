69d853196fbf338f70c86774c77dfd365e89e4fb
Tomasz Święcicki <tomislater@gmail.com>
2013-12-07 17:45:08 +0100

    Fix test http coordinated executor
    (python getting executed separately from bash with a new process id)

--- summon_process/tests/executors/test_http_coordinated_executor.py
+++ summon_process/tests/executors/test_http_coordinated_executor.py
@@ -4,13 +4,18 @@ from summon_process.executors import HTTPCoordinatedExecutor
 
 
 class TestHTTPCoordinatedExecutor(TestCase):
+    host = "127.0.0.1"
+    port = "8000"
+
     def test_it_waits_for_process_to_complete_head_request(self):
-        command = 'bash -c "sleep 3 && python -m SimpleHTTPServer"'
-        executor = HTTPCoordinatedExecutor(command, 'http://127.0.0.1:8000/')
+        command = 'bash -c "sleep 3 && exec python -m SimpleHTTPServer"'
+        executor = HTTPCoordinatedExecutor(
+            command, 'http://{0}:{1}/'.format(self.host, self.port)
+        )
         executor.start()
         assert executor.running()
 
-        conn = HTTPConnection('127.0.0.1', '8000')
+        conn = HTTPConnection(self.host, self.port)
         conn.request('GET', '/')
         assert conn.getresponse().status is OK
         conn.close()
