3f7f77df9d4def0abddc2ae1e482f07b2d85bc5c
Mateusz Lenik <mlen@mlen.pl>
2014-02-19 14:50:25 +0100

    Preparing for release 0.1.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='summon_process',
-      version='0.1.2',
+      version='0.1.3',
       description='Process coordinator for tests',
       long_description=file('README.rst').read(),
       url='https://github.com/mlen/summon_process',
