87e104d6d461cdbb5d1aecdd2867a50fae25c451
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2014-08-03 21:41:51 +0200

    add failing test to illustrate terminate shell=True executors - refs #27

--- tests/executors/test_http_executor.py
+++ tests/executors/test_http_executor.py
@@ -1,6 +1,8 @@
 """HTTP Executor tests."""
 import os
 import sys
+import socket
+
 import pytest
 
 
@@ -42,6 +44,14 @@ def prepare_slow_server_executor(timeout=None):
     )
 
 
+def connect_to_server():
+    """Commong test to check if can connect to server."""
+    conn = HTTPConnection(HOST, PORT)
+    conn.request('GET', '/')
+    assert conn.getresponse().status is OK
+    conn.close()
+
+
 def test_executor_starts_and_waits():
     """Test if process awaits for HEAD request to be completed."""
     command = 'bash -c "sleep 3 && exec python -m {http_server}"'.format(
@@ -54,12 +64,34 @@ def test_executor_starts_and_waits():
     executor.start()
     assert executor.running() is True
 
-    conn = HTTPConnection(HOST, PORT)
-    conn.request('GET', '/')
-    assert conn.getresponse().status is OK
-    conn.close()
+    connect_to_server()
 
     executor.stop()
+
+
+@pytest.mark.skipif(True, reason='Github/mirakuru issue #27')
+def test_shell_started_server_stops():
+    """Test if executor terminates properly executor with shell=True."""
+    command = 'python -m {http_server}'.format(
+        http_server=http_server,
+    )
+    executor = HTTPExecutor(
+        command, 'http://{0}:{1}/'.format(HOST, PORT),
+        timeout=20,
+        shell=True
+    )
+
+    with pytest.raises(socket.error):
+        connect_to_server()
+
+    with executor:
+        assert executor.running() is True
+        connect_to_server()
+
+    assert executor.running() is False
+
+    with pytest.raises(socket.error):
+        connect_to_server()
 
 
 def test_slow_server_starting():
@@ -73,12 +105,7 @@ def test_slow_server_starting():
     executor.start()
     assert executor.running() is True
 
-    conn = HTTPConnection(HOST, PORT)
-    conn.request('GET', '/')
-
-    assert conn.getresponse().status is OK
-
-    conn.close()
+    connect_to_server()
     executor.stop()
 
 
