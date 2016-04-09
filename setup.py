dfbeff30ee4b5948f6358bc74f183eae01b92b26
Paweł Wilczyński <p.wilczynski@clearcode.cc>
2016-04-09 12:55:13 +0200

    Code style enhancements to stop failing on new pylama checks.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ tests_require = (
 )
 extras_require = {
     'docs': ['sphinx'],
-    'tests': tests_require
+    'tests': tests_require,
 }
 
 
@@ -64,7 +64,8 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'License :: OSI Approved :: '
+        'GNU Lesser General Public License v3 or later (LGPLv3+)',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
