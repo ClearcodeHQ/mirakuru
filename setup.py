a7e987e4a7b2b4447ad5665833448ba4c3b7ba9e
Łukasz Kostka <luqasz@users.noreply.github.com>
2016-09-02 12:45:41 +0200

    [requires.io] dependency update on master branch (#124)
    * [requires.io] dependency update
    
    * [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ requirements = [
 ]
 
 tests_require = (
-    'pytest==3.0.1',  # tests framework used
+    'pytest==3.0.2',  # tests framework used
     'pytest-cov==2.3.1',  # coverage reports to verify tests quality
     'mock==2.0.0',  # tests mocking tool
     'python-daemon==2.1.1',  # used in test for easy creation of daemons
