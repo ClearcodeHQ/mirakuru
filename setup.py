8f6f6474a2ad6c28fc6622791dd1bcb57d687e74
requires.io <support@requires.io>
2018-12-14 12:06:26 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ requirements = [
 ]
 
 tests_require = (
-    'pytest==4.0.1',  # tests framework used
+    'pytest==4.0.2',  # tests framework used
     'pytest-cov==2.6.0',  # coverage reports to verify tests quality
     'mock==2.0.0',  # tests mocking tool
     'python-daemon==2.2.0',  # used in test for easy creation of daemons
