3c001ea9a0554b4860e6793d72944bcdcae278d3
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2019-06-05 17:35:24 +0200

    fixup: ignore resource warnings on pypy3.5 on travis

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,15 @@ Tests pre-configuration.
 * Filtering ResourceWarning for the Python 3.
 * Fixture for raising an error whenever we leave any resource open.
 """
-
+import platform
+import os
+import sys
 from warnings import simplefilter
 
 import pytest
+
+is_travis = 'TRAVIS' in os.environ
+is_pypy_35 = platform.python_implementation() == 'PyPy' and sys.version_info < (3, 6)
 
 
 simplefilter(
@@ -20,5 +25,5 @@ simplefilter(
 def error_warn(recwarn):
     """Raise error whenever any warning gets listed."""
     yield
-    if recwarn.list:
+    if recwarn.list and not (is_pypy_35 and is_travis):
         raise recwarn.list[0].message
